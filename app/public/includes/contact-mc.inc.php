e534ca18cf207633ede9baba29d52cec6b315434
Anne S. Katzeff <anne@askdesign.biz>
2026-01-28 21:05:03 -0500

    Adjust contact form code

--- app/public/includes/contact-mc.inc.php
+++ app/public/includes/contact-mc.inc.php
@@ -15,18 +15,24 @@
            We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style> -->
 <div id="mc_embed_signup_c">
-    <form action="https://askdesign.us7.list-manage.com/subscribe/post?u=65a02b0a6ef9c3ca16e54dbf8&amp;id=66d1864e53&amp;f_id=00e045e4f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-        <div id="mc_embed_signup_scroll">
+    <form action="https://askdesign.us7.list-manage.com/subscribe/post?u=65a02b0a6ef9c3ca16e54dbf8&amp;id=66d1864e53&amp;f_id=00e045e4f0" method="post" id="mc-embedded-contact-form" name="mc-embedded-contact-form" class="validate" target="_blank">
+        <div id="mc_embed_contact_scroll">
 
             <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
 
-            <p><div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></p></div>
+            <div class="mc-field-group">
+              <p><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></p></div>
             
-            <p><div class="mc-field-group"><label for="mce-FNAME">First Name <span class="asterisk">*</span></label><input type="text" name="FNAME" class="required text" id="mce-FNAME" required="" value=""></p></div>
+            <div class="mc-field-group">
+              <p><label for="mce-FNAME">First Name <span class="asterisk">*</span></label><input type="text" name="FNAME" class="required text" id="mce-FNAME" required="" value=""></p></div>
             
-            <p><div class="mc-field-group"><label for="mce-LNAME">Last Name <span class="asterisk">*</span></label><input type="text" name="LNAME" class="required text" id="mce-LNAME" required="" value=""></p></div>
+            <div class="mc-field-group">
+              <p><label for="mce-LNAME">Last Name <span class="asterisk">*</span></label><input type="text" name="LNAME" class="required text" id="mce-LNAME" required="" value=""></p></div>
             
-            <p><div class="mc-field-group"><label for="mce-MMERGE4">Message <span class="asterisk">*</span></label><input type="text" name="MMERGE4" class="required text" id="mce-MMERGE4" required="" value=""></p></div>
+          <div class="mc-field-group">
+            <p><label for="mce-MMERGE4">Message <span class="asterisk">*</span></label>
+            <textarea rows="4" name="MMERGE4" class="form-control required text" id="mce-MMERGE4" required="" value=""></textarea></p>
+          </div>
 
 
           <p>This form collects your name and email so that we can reply to you in a professional manner. Please see our <a href="#">Privacy Policy</a> for full details on how we value and protect your privacy. By clicking the SEND button, you are consenting to ASK Design collecting your name and email. </p>
@@ -45,7 +51,7 @@
 
         <div class="optionalParent">
             <div class="clear foot">
-                <input type="submit" name="contact" id="mc-embedded-subscribe" class="btn btn-outline-primary btn-lg mb-3" value="SEND">
+                <input type="submit" name="contact" id="mc-embedded-contact" class="btn btn-outline-primary btn-lg mb-3" value="SEND">
 
                 <p class="mt-3">
                   <a href="http://eepurl.com/ju2rSs" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 150px; height: 40px; display: block; padding: 0px;"></span></a></p>
