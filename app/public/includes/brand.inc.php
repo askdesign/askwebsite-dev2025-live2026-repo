3f2971311030ac810a2b188f88f3fb780204653f
Anne S. Katzeff <anne@askdesign.biz>
2026-07-14 18:58:55 -0400

    Lighthouse Best Practices
    • Added img-fluid to try to improve best practices score

--- app/public/includes/brand.inc.php
+++ app/public/includes/brand.inc.php
@@ -3,7 +3,7 @@
         <div class="col-lg-6 mx-auto"> 
           <div class="gap-4 d-sm-flex justify-content-sm-center align-items-end"> 
             <div class="logo"><a href="../index.php">
-              <img src="/images/images-common/sled3-resize-200.png" alt="logo"></a>
+              <img src="/images/images-common/sled3-resize-200.png" alt="logo" class="img-fluid"></a>
             </div>
 
               <h1 class="title"><a href="../index.php">ASK Design</a></h1>
